c80b419aaf22389c92ed26d5ee67bdb27dd35d2c
kr1pt0n <39812156+kr1pt0n@users.noreply.github.com>
2026-06-23 02:19:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ SECRET_KEY = "SUPER_SECRET_VAULT_KEY_1337"  # Cambia esto por un string aleatori
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 240  # El login dura 4 horas activo
 
-ADMIN_USER = "root"
-ADMIN_PASS = "seguridad"  # Pon aquí la contraseña que usarás para entrar
+ADMIN_USER = "user"
+ADMIN_PASS = "password"  # Pon aquí la contraseña que usarás para entrar
 # =====================================================================
 
 # 1. Automatización de rutas para Docker: Creamos la carpeta 'data' si no existe
@@ -112,3 +112,7 @@ def index():
 @app.get("/ico.png", include_in_schema=False)
 def favicon():
     return FileResponse("ico.png")
+
+@app.get("/logo.png", include_in_schema=False)
+def get_logo():
+    return FileResponse("logo.png")
